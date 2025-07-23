20d4e3be6d25866621cff7d13a2aa29e573c66e8
yulgon <younggon5548@gmail.com>
2025-07-23 13:27:49 +0900

    [feature] 4

--- Project18/baseball.cpp
+++ Project18/baseball.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+struct GuessResult {
+    bool solved;
+    int strikes;
+    int balls;
+};
 class Baseball {
 public:
-    void guess(const string& guessNumber) {
+    explicit Baseball(const string& question) : question(question) {}
+    GuessResult guess(const string& guessNumber) {
         assertIlegalArgument(guessNumber);
+        return { true, 3, 0 };
     }
 
     void assertIlegalArgument(const std::string& guessNumber)
@@ -30,4 +37,6 @@ public:
             || guessNumber[0] == guessNumber[2]
             || guessNumber[1] == guessNumber[2];
     }
+private:
+    string question;
 };
--- Project18/baseball_test.cpp
+++ Project18/baseball_test.cpp
@@ -3,7 +3,7 @@
 
 class BaseballFixture : public testing::Test {
 public:
-    Baseball game;
+    Baseball game{ "123" };
     void assertIllegalArgument(string guessNumber) {
         try {
             game.guess(string(guessNumber));
@@ -25,6 +25,16 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvaildCase)
     assertIllegalArgument("12");
     assertIllegalArgument("12s");
     assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber)
+{
+    //Baseball game("123");
+    GuessResult result = game.guess("123");
+
+    EXPECT_TRUE(result.solved);
+    EXPECT_EQ(3, result.strikes);
+    EXPECT_EQ(0, result.balls);
 }
 
 int main() {
