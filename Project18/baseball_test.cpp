7c40d042da2aac222adcd8714bd3548b43ae501f
yulgon <younggon5548@gmail.com>
2025-07-23 12:42:13 +0900

    first commit

---
+++ Project18/baseball_test.cpp
@@ -0,0 +1,12 @@
+#include "gmock/gmock.h"
+#include "baseball.cpp"
+
+
+TEST(BaseballGame, TryGameTest) {
+    EXPECT_EQ(1, 1);
+}
+
+int main() {
+    testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
+}
